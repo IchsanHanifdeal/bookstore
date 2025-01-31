f119a9c7b2152ef6a7a126cd8e0abf91db8b28fe
IchsanHanifdeal <ichsanhanifdeal@gmail.com>
2025-01-31 15:48:41 +0700

    lupa masukin close all modals

--- resources/views/dashboard/transaksi.blade.php
+++ resources/views/dashboard/transaksi.blade.php
@@ -257,7 +257,7 @@
                         onclick="document.getElementById('{{ $action }}_modal_{{ $item->id }}').close();"
                         class="btn btn-ghost text-gray-500 hover:text-gray-700 focus:outline-none transition duration-200">Tutup</button>
                     <button type="submit"
-                        class="btn btn-primary capitalize bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md transition-all duration-300">
+                        class="btn btn-primary capitalize bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md transition-all duration-300" onclick="closeAllModals(event)">
                         {{ ucfirst($action) }}
                     </button>
                 </div>
